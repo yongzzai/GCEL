0f1eb56bf581fcb0a79d96793fb1823b1123c724
yongzzai <yongzzai1102@gmail.com>
2025-03-18 10:02:20 +0900

    rename directory.py to fs.py

---
+++ Modules/Converter.py
@@ -0,0 +1,11 @@
+'''
+@author: Y.J.Lee
+'''
+
+from utils.EventLog import EventLog
+
+class Converter(EventLog):
+
+    def __init__(self):
+
+        pass
--- utils/EventLog.py
+++ utils/EventLog.py
@@ -1,22 +1,34 @@
 import pandas as pd
-from utils.directory import EVENTLOG_DIR
+from utils.fs import EVENTLOG_DIR
 import os
 from pathlib import Path
 
 
 class EventLog(object):
 
-    def __init__(self, LogName):
-        
+    def __init__(self, LogName):        
         self.log = self.load(LogName)
     
     def load(self, LogName):
 
         LogName = Path(LogName)
-
         if '.csv' not in LogName.suffixes:
             FileName = Path(str(LogName) + '.csv')
-        
         FilePath = os.path.join(EVENTLOG_DIR, FileName)
-
         return pd.read_csv(FilePath)
+
+    @property
+    def cases(self):
+        return [x for x in self.log['case_id'].unique()]
+    
+    @property
+    def case_len(self):
+        pass
+
+    @property
+    def max_case_len(self):
+        pass
+
+    @property
+    def feature(self):
+        pass
