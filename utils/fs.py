ff2aa0dce43761e0570e77820148ac716dba768f
yongzzai <yongzzai1102@gmail.com>
2025-04-07 20:02:26 +0900

    event log for clustering prepared

--- utils/fs.py
+++ utils/fs.py
@@ -15,21 +15,25 @@ ATTR_KEYS = {
     'BPIC13_closed_problems':{'AttributeKeys':['name', 'org_group', 'org_resource', 'organization_country', 'resource_country']},
     'BPIC13_incidents':{'AttributeKeys':['name', 'org_group', 'org_resource', 'organization_country', 'resource_country']},
     'BPIC13_open_problems':{'AttributeKeys':['name', 'org_group', 'org_resource']},
+    'BPIC13c':{'AttributeKeys':['name', 'org_group', 'org_resource']},
     
     'BPIC15_1':{'AttributeKeys':['name', 'monitoringResource', 'org_resource']},
     'BPIC15_2':{'AttributeKeys':['name', 'monitoringResource', 'org_resource']},
     'BPIC15_3':{'AttributeKeys':['name', 'action_code', 'monitoringResource', 'org_resource']},
     'BPIC15_4':{'AttributeKeys':['name', 'monitoringResource', 'org_resource']},
     'BPIC15_5':{'AttributeKeys':['name', 'monitoringResource', 'org_resource']},
+    'BPIC15c':{'AttributeKeys':['name', 'monitoringResource', 'org_resource']},
 
     'BPIC17':{'AttributeKeys':['name', 'org_resource']},
     'BPIC17_offer':{'AttributeKeys':['name', 'org_resource']},
+    'BPIC17c':{'AttributeKeys':['name', 'org_resource']},
 
     'BPIC20_DomesticDeclarations':{'AttributeKeys':['name', 'org_resource', 'org_role']},
     'BPIC20_InternationalDeclarations':{'AttributeKeys':['name', 'org_resource', 'org_role']},
     'BPIC20_PermitLog':{'AttributeKeys':['name', 'org_resource', 'org_role']},
     'BPIC20_PrepaidTravelCost':{'AttributeKeys':['name', 'org_resource', 'org_role']},
     'BPIC20_RequestForPayment':{'AttributeKeys':['name', 'org_resource', 'org_role']},
+    'BPIC20c':{'AttributeKeys':['name', 'org_resource', 'org_role']},
 
     'Receipt':{'AttributeKeys':['name', 'org_group', 'org_resource']},
     'Sepsis':{'AttributeKeys':['name','org_group']},
